6624483d56179e054762ff502ef8e1c1dee5e279
David Elizondo <daveligu@gmail.com>
2016-11-16 13:22:04 -0600

    udpate

--- Semana7/MyApplication/app/src/main/java/llbeam/myapplication/ContactsActivity.java
+++ Semana7/MyApplication/app/src/main/java/llbeam/myapplication/ContactsActivity.java
@@ -4,42 +4,66 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * Created by david on 11/16/16.
  */
 
-public class ContactsActivity extends AppCompatActivity {
+public class ContactsActivity extends AppCompatActivity implements ContactsListener {
 
 
     private static final String TAG = ContactsActivity.class.getName();
 
     private static final int CONTACTS_PERSMISSIONS_CODE = 100;
 
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
+    private ContactsListener mListener;
+
+    private RecyclerView mRecyclerView;
 
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.contacts_activity_layout);
+        mListener = this;
+        mRecyclerView = (RecyclerView) findViewById(R.id.list);
 
-        if (hasReadContactsPermission() != PackageManager.PERMISSION_GRANTED) {
-            // Need to request permissions
-            askForPermissions();
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setHasFixedSize(true);
+
+        int dividerHeight = getResources().getDimensionPixelSize(R.dimen.divider);
+        DividerDecorator dividerDecorator = new DividerDecorator(dividerHeight);
+        mRecyclerView.addItemDecoration(dividerDecorator);
+
+
+        if (hasMarshmallow()) {
+            if (hasReadContactsPermission() != PackageManager.PERMISSION_GRANTED) {
+                // Need to request permissions
+                askForPermissions();
+            } else {
+                loadContactsWithCellPhone();
+            }
         } else {
             loadContactsWithCellPhone();
         }
+
 
     }
 
@@ -66,8 +90,14 @@ public class ContactsActivity extends AppCompatActivity {
         }
     }
 
+    public static boolean hasMarshmallow() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.M;
+    }
+
 
     private void loadContactsWithCellPhone() {
+
+        List<MyContact> contacts = new ArrayList<>();
 
         Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
         String[] projection = {ContactsContract.CommonDataKinds.Phone._ID, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
@@ -87,9 +117,16 @@ public class ContactsActivity extends AppCompatActivity {
                 String number = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
                 String contactId = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone._ID));
                 Log.d(TAG, contactId + " " + contactName + " " + number);
+                contacts.add(new MyContact(contactName, number, contactId));
             }
             cursor.close();
+            mListener.onContactsLoaded(contacts);
         }
     }
 
+    @Override
+    public void onContactsLoaded(List<MyContact> list) {
+        MyContactAdapter adapter = new MyContactAdapter(this, list);
+        mRecyclerView.setAdapter(adapter);
+    }
 }
---
+++ Semana7/MyApplication/app/src/main/java/llbeam/myapplication/ContactsListener.java
@@ -0,0 +1,13 @@
+package llbeam.myapplication;
+
+import java.util.List;
+
+/**
+ * Created by david on 11/16/16.
+ */
+
+public interface ContactsListener {
+
+    void onContactsLoaded(List<MyContact> list);
+
+}
