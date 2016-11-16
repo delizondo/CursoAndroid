6043ee180568aea4e99f1911021eb5312993e9d3
David Elizondo <daveligu@gmail.com>
2016-11-16 10:09:37 -0600

    update

---
+++ Semana7/MyApplication/app/src/main/java/llbeam/myapplication/ContactsActivity.java
@@ -0,0 +1,18 @@
+package llbeam.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by david on 11/16/16.
+ */
+
+public class ContactsActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
