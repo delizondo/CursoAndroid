bf486efdaf83523e46ca25c70ac6e273900a24d2
David Elizondo <daveligu@gmail.com>
2016-11-23 09:08:47 -0600

    add volley cache

--- Semana8/Volley/MyVolleyApp/app/src/main/java/myapplication/myvolleyapp/api/ApiClient.java
+++ Semana8/Volley/MyVolleyApp/app/src/main/java/myapplication/myvolleyapp/api/ApiClient.java
@@ -2,10 +2,14 @@ package myapplication.myvolleyapp.api;
 
 import android.content.Context;
 
+import com.android.volley.Cache;
+import com.android.volley.Network;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.ImageLoader;
-import com.android.volley.toolbox.Volley;
 
 /**
  * Created by david on 11/22/16.
@@ -36,9 +40,19 @@ public class ApiClient {
 
     public RequestQueue getRequestQueue() {
         if (mRequestQueue == null) {
+
+            // Instantiate the cache
+            Cache cache = new DiskBasedCache(mContext.getCacheDir(), 1024 * 1024); // 1MB cap
+            // Set up the network to use HttpURLConnection as the HTTP client.
+            Network network = new BasicNetwork(new HurlStack());
+            // Instantiate the RequestQueue with the cache and network.
+            mRequestQueue = new RequestQueue(cache, network);
+            // Start the queue
+            mRequestQueue.start();
+
             // getApplicationContext() is key, it keeps you from leaking the
             // Activity or BroadcastReceiver if someone passes one in.
-            mRequestQueue = Volley.newRequestQueue(mContext.getApplicationContext());
+            //mRequestQueue = Volley.newRequestQueue(mContext.getApplicationContext());
         }
         return mRequestQueue;
     }
