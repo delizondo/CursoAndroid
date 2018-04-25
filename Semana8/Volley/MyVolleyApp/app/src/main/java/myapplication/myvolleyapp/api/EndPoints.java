c81c52a8fe7848f74753914fa63f2b63ffb9fe0a
David Elizondo <daveligu@gmail.com>
2018-04-25 10:22:20 -0600

    update volley app

--- Semana8/Volley/MyVolleyApp/app/src/main/java/myapplication/myvolleyapp/api/EndPoints.java
+++ Semana8/Volley/MyVolleyApp/app/src/main/java/myapplication/myvolleyapp/api/EndPoints.java
@@ -9,7 +9,7 @@ public class EndPoints {
     private static final String ENDPOINT = "http://www.omdbapi.com/";
 
     public static String getMoviesEndpoint(String query) {
-        return ENDPOINT + "?y=&plot=short&r=json" + "&s=" + query;
+        return ENDPOINT + "?y=&plot=short&apikey=6cbcbe3c&r=json" + "&s=" + query;
     }
 
 }
